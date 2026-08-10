6bf1a8570edf33e630f4aa75064808e118b848ad
JKimotho-254 <josephkimothonurse@gmail.com>
2026-08-10 18:43:17 +0300

    Add Supabase configuration file

---
+++ js/supabase-config.js
@@ -0,0 +1,3 @@
+const SUPABASE_URL = 'https://jsbaijvirzipctetbmui.supabase.co';
+const SUPABASE_ANON_KEY = 'Sb_publishable_BkJAsMhhprPc4e2Xwh0xcw_5EiLGCxR';
+const supabase = window.supabase.createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
